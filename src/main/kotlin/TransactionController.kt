8c3d47ce512f12e6fe0638731c7bb3601e775d47
Paula Eduarda Justino <rpaulaeduarda@gmail.com>
2023-09-09 17:04:50 -0300

    refactor on class TransactionController

--- src/main/kotlin/TransactionController.kt
+++ src/main/kotlin/TransactionController.kt
@@ -11,64 +11,50 @@ class TransactionController(
             // busca conta relacionada a transação
             val account = accountController.getAccountById(accountId = accountId)
 
-            // verifica se conta existe
-            if (account != null) {
-
-                // verifica se saldo da conta é suficiente para fazer a transação
-                val updatedBalance = calculateIfAccountHasSufficientBalance(transaction = this, account = account)
-                if (updatedBalance != null) {
-
-                    // atualiza saldo da conta
-                    val balanceType = checkBalanceTypeUsedInTransaction(transactionMcc = mcc)
-                    accountController.updateBalanceAccount(
-                        accountId = account.id,
-                        balance = updatedBalance,
-                        balanceType = balanceType
-                    )
-
-                    // registra resultado da transação no banco
-                    transactionResult = TransactionResult(
-                        id = id,
-                        accountId = accountId,
-                        amount = amount,
-                        merchant = merchant,
-                        mcc = mcc,
-                        status = TransactionStatus.APPROVED.status,
-                        message = TransactionStatusMessage.APPROVED_MESSAGE.statusMessage,
-                    )
-
-                    transactionResultController.insertTransactionResult(transactionResult)
-                } else {
-                    transactionResult = TransactionResult(
-                        id = id,
-                        accountId = accountId,
-                        amount = amount,
-                        merchant = merchant,
-                        mcc = mcc,
-                        status = TransactionStatus.DENIED.status,
-                        message = TransactionStatusMessage.INSUFFICIENT_BALANCE_MESSAGE.statusMessage,
-                    )
-
-                    transactionResultController.insertTransactionResult(transactionResult)
-                }
-
-            } else {
-                transactionResult = TransactionResult(
-                    id = id,
-                    accountId = accountId,
-                    amount = amount,
-                    merchant = merchant,
-                    mcc = mcc,
-                    status = TransactionStatus.DENIED.status,
-                    message = TransactionStatusMessage.INVALID_ACCOUNT_ID_MESSAGE.statusMessage,
+            // verifica se a conta existe e registra o resultado da transação
+            if (account == null) {
+                transactionResult = createTransactionResult(
+                    transaction = this,
+                    status = TransactionStatus.DENIED,
+                    message = TransactionStatusMessage.INVALID_ACCOUNT_ID_MESSAGE
                 )
-
                 transactionResultController.insertTransactionResult(transactionResult)
+                return
             }
+
+            // calcula novo saldo da conta
+            val updatedBalance = hasAccountSufficientBalance(transaction = this, account = account)
+
+            // verifica se a conta tem saldo suficiente e registra o resultado da transação
+            if (updatedBalance == null) {
+                transactionResult = createTransactionResult(
+                    transaction = this,
+                    status = TransactionStatus.DENIED,
+                    message = TransactionStatusMessage.INSUFFICIENT_BALANCE_MESSAGE
+                )
+                transactionResultController.insertTransactionResult(transactionResult)
+                return
+            }
+
+            // atualiza o saldo da conta e registra o resultado da transação
+            val balanceType = checkBalanceTypeUsedInTransaction(transactionMcc = mcc)
+            accountController.updateBalanceAccount(
+                accountId = account.id,
+                balance = updatedBalance,
+                balanceType = balanceType
+            )
+
+            transactionResult = createTransactionResult(
+                transaction = this,
+                status = TransactionStatus.APPROVED,
+                message = TransactionStatusMessage.APPROVED_MESSAGE
+            )
+
+            transactionResultController.insertTransactionResult(transactionResult)
         }
     }
 
-    private fun calculateIfAccountHasSufficientBalance(transaction: Transaction, account: Account): Double? {
+    private fun hasAccountSufficientBalance(transaction: Transaction, account: Account): Double? {
         return when (transaction.mcc) {
             "5411", "5412" -> calculateAccountBalance(transaction.amount, account.foodBalance)
             "5811", "5812" -> calculateAccountBalance(transaction.amount, account.mealBalance)
@@ -92,5 +78,21 @@ class TransactionController(
             "5811", "5812" -> "mealBalance"
             else -> "cashBalance"
         }
+    }
+
+    private fun createTransactionResult(
+        transaction: Transaction,
+        status: TransactionStatus,
+        message: TransactionStatusMessage
+    ): TransactionResult {
+        return TransactionResult(
+            id = transaction.id,
+            accountId = transaction.accountId,
+            amount = transaction.amount,
+            merchant = transaction.merchant,
+            mcc = transaction.mcc,
+            status = status.status,
+            message = message.statusMessage
+        )
     }
 }
